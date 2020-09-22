57cf3831e537d384c91727024c26fe1a04d328b9
i1rr <i1r@tuta.io>
2020-09-22 14:29:49 +1000

    9.1. String [#368435]

---
+++ src/ru/job4j/calculate/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculate;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "L like Java!";
+        System.out.println(idea);
+        idea = idea + " But I am a newbie.";
+        int year = 2021;
+        idea = idea + " (c)" + year;
+        System.out.println(idea);
+    }
+}
