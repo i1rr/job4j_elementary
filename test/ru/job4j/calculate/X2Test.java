6cf0bdeeed6343fb83b771b149aea0ebba869125
i1rr <i1r@tuta.io>
2020-09-22 21:26:04 +1000

    1.1. Входные данные в тесте [#368462]

---
+++ test/ru/job4j/calculate/X2Test.java
@@ -0,0 +1,53 @@
+package ru.job4j.calculate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class X2Test {
+
+    @Test
+    public void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int out = X2.calc(a, b, c, x);
+
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenA0B1C1X1Then3() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int out = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenA1B1C0X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int out = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenA1B1C1X0Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int out = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, out);
+    }
+}
+//return a * x * x + b * x + c;
