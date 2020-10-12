1da27f093fa421d4e30ca54512c73b2b01b0eb43
i1rr <i1r@tuta.io>
2020-10-12 19:41:13 +1100

    4.3.1. Оператор && [#368379]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -10,8 +10,16 @@ public class Cinema {
         }
     }
 
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        System.out.println(allowByParent && hasMoney ? "I can go to cinema." : "I can't.");
+    }
+
     public static void main(String[] args) {
-        Cinema.access(21);
-        Cinema.access(16);
+       // Cinema.access(21);
+        // Cinema.access(16);
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
     }
 }
