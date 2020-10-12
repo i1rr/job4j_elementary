46d66b2c2a5f4b1ba88f4e84936d6162b2a6fcc8
i1rr <i1r@tuta.io>
2020-10-12 19:48:05 +1100

    4.3.2. Оператор || [#368380]

---
+++ src/ru/job4j/condition/Club.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Club {
+    public static void permission(boolean hasMoney, boolean beFriend) {
+        System.out.println(hasMoney || beFriend ? "I can go to the club." : "I can't");
+    }
+
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+    }
+
+}
