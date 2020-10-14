770f04620b169b69e1a5a742448907f3efc6bf56
i1rr <i1r@tuta.io>
2020-10-14 22:08:35 +1100

    5.4. Протеиновая диета [#368481]

---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+class FitnessTest {
+
+    @Test
+    void whenI95N90Month0() {
+        int month = Fitness.calc(95, 90);
+        int expectation = 0;
+        assertThat(month, is(expectation));
+    }
+
+    @Test
+    void whenI90N95Month1() {
+        int month = Fitness.calc(90, 95);
+        int expectation = 1;
+        assertThat(month, is(expectation));
+    }
+
+    @Test
+    void whenI50N90Month2() {
+        int month = Fitness.calc(50, 90);
+        int expectation = 2;
+        assertThat(month, is(expectation));
+    }
+
+}
