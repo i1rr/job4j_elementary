6a4753673eef320bcf936a59129e427ae95164b5
i1rr <i1r@tuta.io>
2020-10-21 22:00:14 +1100

    5.9. Крест в псевдографике. [#368485]

---
+++ src/ru/job4j/loop/Slash.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import java.sql.SQLOutput;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 1; row <= size; row++) {
+            for (int cell = 1; cell <= size; cell++) {
+                if (row == cell || cell == (size - row + 1)) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println();
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
