ca2832d315093d6bd9d7c1109bd0e2c6930070a5
i1rr <i1r@tuta.io>
2020-10-22 21:08:54 +1100

    6.0.2. Заполнение массива. [#368406]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Fedja";
+        names[1] = "Petja";
+        names[2] = "Stjopa";
+        names[3] = "Volodja";
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println();
+
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
