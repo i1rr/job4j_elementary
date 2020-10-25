0d070fdf9dd54db7e3946f562d2aba7f2072d430
i1rr <i1r@tuta.io>
2020-10-25 19:50:32 +1100

    6.6.1. Двухмерный массив. Вывод на консоль. [#368412]

--- src/ru/job4j/array/MinDiapason.java
+++ src/ru/job4j/array/MinDiapason.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class MinDiapason {
     public static int findMin(int[] array, int start, int finish) {
-        int result = Integer.MAX_VALUE;
+        int result = start;
         for (int i = start; i <= finish; i++) {
             if (result > array[i]) {
                 result = array[i];
