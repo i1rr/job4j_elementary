10283486490542b8db88fb0e10e79b98ff947091
i1rr <i1r@tuta.io>
2020-11-05 20:11:17 +1100

    3. Перегрузить метод max для трех чисел [#368583]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -4,4 +4,14 @@ public class Max {
     public static int max(int left, int right) {
         return left >= right ? left : right;
     }
+
+    public static int max(int one, int two, int three) {
+        int temp = max(one, two);
+        return max(temp, three);
+    }
+
+    public static int max(int one, int two, int three, int four) {
+        int temp = max(one, two, three);
+        return max(temp, four);
+    }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -23,4 +23,28 @@ public class MaxTest {
         int result = Max.max(9, 9);
         assertThat(result, is(9));
     }
+
+    @Test
+    public void whenMax9among129() {
+        int result = Max.max(1, 2, 9);
+        assertThat(result, is(9));
+    }
+
+    @Test
+    public void whenMax7among177() {
+        int result = Max.max(1, 7, 7);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMax6among1226() {
+        int result = Max.max(1, 2, 2, 6);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenMax19among11119() {
+        int result = Max.max(1, 1, 1, 19);
+        assertThat(result, is(19));
+    }
 }
