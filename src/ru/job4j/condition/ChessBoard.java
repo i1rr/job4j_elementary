e85a533cb306c26047ac36f54a8e70680f6f17fd
i1rr <i1r@tuta.io>
2021-05-19 20:56:22 +1000

    Math.abs and bishop on chessBoard bugFix) (updated trainee tasks)

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -16,6 +16,6 @@ public class ChessBoard {
     }
 
     public static boolean bishopSteps(int x1, int y1, int x2, int y2) {
-        return Math.abs(x1 - y1) == Math.abs(x2 - y2);
+        return Math.abs(x1 - x2) == Math.abs(y1 - y2);
     }
 }
